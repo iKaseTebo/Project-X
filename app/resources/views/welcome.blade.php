b22320ccd8df32674911aebdb94d38c2102c9cac
Chris Manfredi <ChrisManfredi11@gmail.com>
2017-10-23 14:06:40 -0400

    google signin

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <meta name="google-signin-client_id" content="862262554506-mnr23l6d4tbm5atg5b72eo5df7b5p1ta.apps.googleusercontent.com">
+
+        <title>Project X</title>
 
         <link rel="stylesheet" href="http://localhost:8080/css/app.css">
         <link rel="stylesheet" href="http://localhost:8080/css/font-awesome.min.css">
@@ -13,7 +15,7 @@
     </head>
     <body ng-view>
 
-    </body>aa
+    </body>
     <script type="text/javascript" src="http://localhost:8080/js/jquery.js"></script>
     <script type="text/javascript" src="http://localhost:8080/js/materialize.js"></script>
     <script type="text/javascript" src="http://localhost:8080/js/init.js"></script>
